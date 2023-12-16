a7b938490b30c1c527b723558f9ba640f688e713
Scott Forsmann <forsmannscott@gmail.com>
2023-12-16 14:03:24 -0800

    Day 11

---
+++ Day11/day11b.py
@@ -0,0 +1,102 @@
+def get_data():
+    data = open('data.txt').read().split('\n')
+    return data
+
+
+def get_rows_cols(data):
+    galaxy_rows = set()
+    galaxy_cols = set()
+    empty_cols = set()
+    empty_rows = set()
+
+    for r in range(len(data)):
+        for c in range(len(data[r])):
+            if data[r][c] == '#':
+                galaxy_rows.add(r)
+                galaxy_cols.add(c)
+        if r not in galaxy_rows:
+            empty_rows.add(r)
+
+    for c in range(len(data[0])):
+        if c not in galaxy_cols:
+            empty_cols.add(c)
+
+    return galaxy_rows, galaxy_cols, empty_rows, empty_cols
+
+
+def expand_universe(data):
+    galaxy_rows, galaxy_cols, empty_rows, empty_cols = get_rows_cols(data)
+    # Expand columns first, then rows
+    for r in range(len(data)):
+        if r in empty_rows:
+            for c in range(len(data[r])):
+                if c in empty_cols:
+                    data[r] = data[r][:c] + '2' + data[r][c+1:]
+                else:
+                    data[r] = data[r][:c] + '1' + data[r][c+1:]
+        else:
+            for c in empty_cols:
+                data[r] = data[r][:c] + '1' + data[r][c+1:]
+
+    return data
+
+
+def get_all_galaxy_coordinates(data):
+    galaxy_coordinates = []
+    for r in range(len(data)):
+        for c in range(len(data[r])):
+            if data[r][c] == '#':
+                galaxy_coordinates.append((r, c))
+
+    return galaxy_coordinates
+
+
+def get_all_pairs(l):
+    perms = []
+    for i in range(len(l)):
+        for j in range(i+1, len(l)):
+            perms.append((l[i], l[j]))
+
+    return perms
+
+
+def calculate_shortest_path_sum(data):
+    galaxy_coordinates = get_all_galaxy_coordinates(data)
+    galaxy_permutations = get_all_pairs(galaxy_coordinates)
+    galaxy_rows, galaxy_cols, empty_rows, empty_cols = get_rows_cols(data)
+
+    sum_lengths = 0
+    offset = 1000000
+
+    for g1, g2 in galaxy_permutations:
+        r_max, r_min = max(g1[0], g2[0]), min(g1[0], g2[0])
+        c_max, c_min = max(g1[1], g2[1]), min(g1[1], g2[1])
+
+        for r in range(r_min+1, r_max + 1):
+            if r in empty_rows:
+                sum_lengths += offset
+            else:
+                sum_lengths += 1
+
+        for c in range(c_min+1, c_max + 1):
+            if c in empty_cols:
+                sum_lengths += offset
+            else:
+                sum_lengths += 1
+
+
+        # sum_lengths += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+
+    return sum_lengths
+
+
+
+def main():
+    data = get_data()
+    # data = expand_universe(data)
+
+    res = calculate_shortest_path_sum(data)
+    print(res)
+
+if __name__ == "__main__":
+    main()
